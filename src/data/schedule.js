c07aeb54ab55785191fd4b1570c3ee9f673ea6a3
Voravich S <vsilapac@gmail.com>
2025-10-17 02:57:38 -0400

    Update schedule.js

--- src/data/schedule.js
+++ src/data/schedule.js
@@ -5,13 +5,13 @@ const schedule = [
   { id: 4, date: "9/29", topic: "HTML, CSS, and the DOM", slides: "/slides/Lec4_1962.pdf", code: "/code/lec4/lec4.zip", assignmentsAssigned: "HW2", assignmentsDue: "HW1" },
   { id: 5, date: "10/6", topic: "Object-Oriented Programming and Design Patterns", slides: "/slides/Lec5_1962.pdf", code: "/code/lec5/lec5.zip", assignmentsAssigned: "", assignmentsDue: "" },
   { id: "-", date: "10/13", topic: "NO CLASS (Indigenous People's Day)", slides: "", code: "", assignmentsAssigned: "", assignmentsDue: "" },
-  { id: 6, date: "10/20", topic: "Project Management: Modules, Packages, Testing, and Linting", slides: "", code: "", assignmentsAssigned: "HW3", assignmentsDue: "HW2" },
-  { id: 7, date: "10/27", topic: "Exploration of Contemporary JavaScript Topics", slides: "", code: "", assignmentsAssigned: "", assignmentsDue: "" },
-  { id: 8, date: "11/3", topic: "Exploration of Contemporary JavaScript Topics", slides: "", code: "", assignmentsAssigned: "Final Project and HW4", assignmentsDue: "HW3" },
-  { id: 9, date: "11/10", topic: "Exploration of Contemporary JavaScript Topics", slides: "", code: "", assignmentsAssigned: "", assignmentsDue: "Final Project Proposals" },
-  { id: 10, date: "11/17", topic: "Exploration of Contemporary JavaScript Topics", slides: "", code: "", assignmentsAssigned: "HW5", assignmentsDue: "HW4" },
-  { id: 11, date: "11/24", topic: "Exploration of Contemporary JavaScript Topics", slides: "", code: "", assignmentsAssigned: "", assignmentsDue: "Final Project Check-In" },
-  { id: 12, date: "12/1", topic: "Exploration of Contemporary JavaScript Topics", slides: "", code: "", assignmentsAssigned: "", assignmentsDue: "HW5 Due" },
+  { id: 6, date: "10/20", topic: "Project Management & Introduction to TypeScript", slides: "", code: "", assignmentsAssigned: "HW3", assignmentsDue: "HW2" },
+  { id: 7, date: "10/27", topic: "TypeScript", slides: "", code: "", assignmentsAssigned: "", assignmentsDue: "" },
+  { id: 8, date: "11/3", topic: "React", slides: "", code: "", assignmentsAssigned: "Final Project and HW4", assignmentsDue: "HW3" },
+  { id: 9, date: "11/10", topic: "React", slides: "", code: "", assignmentsAssigned: "", assignmentsDue: "Final Project Proposals" },
+  { id: 10, date: "11/17", topic: "Building an API with Express.js", slides: "", code: "", assignmentsAssigned: "HW5", assignmentsDue: "HW4" },
+  { id: 11, date: "11/24", topic: "Backend, MongoDB, and Cloud Integration", slides: "", code: "", assignmentsAssigned: "", assignmentsDue: "Final Project Check-In" },
+  { id: 12, date: "12/1", topic: "Authorization, Security, and Webpage Monitoring", slides: "", code: "", assignmentsAssigned: "", assignmentsDue: "HW5 Due" },
   { id: 13, date: "12/8", topic: "Final Project Presentations", slides: "", code: "", assignmentsAssigned: "", assignmentsDue: "Final Project Due" }
 ];
 
