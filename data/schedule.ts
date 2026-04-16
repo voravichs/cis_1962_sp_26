e0345cc24e2f0843923fa9a2db101c903a152e78
Voravich S <vsilapac@gmail.com>
2026-04-16 03:32:18 -0400

    lec13

--- data/schedule.ts
+++ data/schedule.ts
@@ -180,9 +180,9 @@ const schedule = [
   {
     id: 13,
     date: "4/16",
-    topic: "TBD",
-    slides: "",
-    code: "",
+    topic: "The State of JavaScript",
+    slides: "/lectures/Lec13_2962_sp26.pdf",
+    code: "https://edstem.org/us/courses/91614/lessons/164119/slides/965210",
     homework:   { 
       id: 'Final Project',
       title: "Milestone",
